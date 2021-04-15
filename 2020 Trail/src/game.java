8c6538800dc0de389134f136c11731f6135441c0
abiedavi <82478845+abiedavi@users.noreply.github.com>
2021-04-15 16:02:59 -0400

    First Upload!
    Let's see if this works. I already have a game class that I will be working on and it automatically created a main method in the class Trail

---
+++ 2020 Trail/src/game.java
@@ -0,0 +1,22 @@
+public class game
+{
+    private String playerName;
+    private int playerScore;
+    
+    
+    //method to determine your outcome based on your score
+    //input the user's score and output what happened to them
+    public String determineOutcome(int score)
+    {
+        String result = "";
+        if(score < 0)
+        {
+            
+        }
+        return result;
+    }
+    
+    //method to display high score at the end of the game
+    
+  
+}
