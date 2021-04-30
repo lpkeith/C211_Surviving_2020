a7b2493a7b1e6fbd0dda749198915fef505b4324
MaggieMHagan <82478061+MaggieMHagan@users.noreply.github.com>
2021-04-29 21:51:29 -0500

    Add files via upload

---
+++ PlayGame.java
@@ -0,0 +1,37 @@
+package GUI;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import GUI.TitleScreen;
+import GUI.QuestionScreen1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author abi
+ */
+public class PlayGame 
+{
+    public static void main(String[] args)
+    {
+       // Scanner scan = new Scanner(System.in); //create a scanner object   
+        
+       // game game = null;               
+       // ArrayList<PlayerScore> listOfPlayers = game.getList(); //gets list from game class and stores in an ArrayList of the same name
+        
+        
+        
+      //  listOfPlayers.add(newPlayer(score));
+        
+        TitleScreen.showTitleScreen();
+       
+        
+    
+    }
+}
